f3baf7b08ef897acba918740581ef637bccf50a8
abood265 <abdsalah234@gmail.com>
2021-01-29 15:50:16 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './icon.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         King Abd Salah
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         My Facebook account
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
